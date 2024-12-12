bae0276653efd8f2933ac1f7a7253fe6d33ba354
Alej426 <laguilera@uniempresarial.edu.co>
2024-12-12 07:57:54 -0500

    Add files via upload

---
+++ entidadfinaciera-main/EntidadFinanciera-main/src/main/java/co/edu/EntidadFinanciera/EntidadFinancieraApplication.java
@@ -0,0 +1,17 @@
+package co.edu.EntidadFinanciera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadFinancieraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadFinancieraApplication.class, args);
+	}
+
+}
+//alejandro aguilera
+//eidan cardenas
+//fredy estupiñan
+
